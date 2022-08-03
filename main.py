58e10e9d8635879f763f3b9da11e1b9278cf6c65
Viktor Skotnikov <Skotnikov2007@gmail.com>
2022-08-03 18:46:50 +0300

    Добавлены функция фото

--- main.py
+++ main.py
@@ -2,12 +2,20 @@
 # Импорт библиотеки telebot и time
 import telebot
 import json
+import time
+import cv2  
 # Создание переменной с токеном
 bot = telebot.TeleBot('5505530308:AAGoiiUP5dD6GP6eM_3b5AfHJQrVdDXFXQI')
 
 with open('file.json','r') as file:
     a = json.load(file)
-    
+
+
+def cinema():
+    cap = cv2.VideoCapture(0)
+    ret,frame = cap.read()
+    cv2.imwrite('cinemabot.png',frame )
+    cap.release()
 
 # Создание кнопки в чате [start] + вложенная функция
 @bot.message_handler(commands = ['start'])
@@ -31,6 +39,14 @@ def get_user_text(message):
         bot.send_message(message.chat.id, 'BotOnPython!')
     elif message.text == 'Калькулятор!':
         bot.send_message(message.chat.id,'Выберите сложение, вычитание, умножение или деление')
+    elif message.text == "/help":
+        bot.send_message(message.chat.id,'Я буду рад вам помочь!)')
+    elif message.text == "Фото":
+        bot.send_message(message.chat.id,'Обратный отсчет 3....2....1...')
+        time.sleep(3)
+        cinema()
+        bot.send_message(message.chat.id,'Фото сделано')
+
     else:
         bot.send_message(message.chat.id,'Опа, а вот это уже не понятно!')
 
