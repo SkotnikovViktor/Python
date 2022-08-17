e40a78961069e28e7bef2495ed7e6c2ad5eb98dd
Viktor <Skotnikov2007@gmail.com>
2022-08-17 18:59:36 +0300

    Добавлена кнопка

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+a = 0
+b = True
+def click():
+    global a
+    a += 1
+    print(a)
+    if a == 5:
+        print("5!!!")
+
 import tkinter as tk
 root = tk.Tk()
+root.title("Программа для кликанья")
+btn = tk.Button(root,text = "Кликни на мне срочно", command=click)
+
 root.geometry("500x500")
+btn.pack()
 root.mainloop()
