e7c50885c3b0122deeb0e7ed4239924c79b83f00
viktor <skotnikov2007@gmail.com>
2022-08-26 21:54:02 +0300

    Добавлен подсчет кликов и их вывод

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 a = 0
+
 def click():
     global a
     a += 1
-    print(a)
+    
+
+def end():
+    txt = tk.Label(text = a)
+    txt.pack()
+    return txt
 
 
-# Импорт библиотеки
+
 import tkinter as tk
+
 root = tk.Tk()
 root.title("Программа для кликанья")
+
+
 btn = tk.Button(root,text = "Кликни на мне срочно", command=click)
 btn.pack()
+btn2 = tk.Button(root,text = "Посчитаить сколько всего кликов", command = end)
+btn2.pack()
+
+
 root.geometry("500x500")
 root.mainloop()
