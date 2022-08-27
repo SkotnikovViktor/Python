95e1559c0999d349c700e513f6233c577d50cbb5
viktor <skotnikov2007@gmail.com>
2022-08-27 17:43:32 +0300

    hot fix

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-a = 0
 
+import tkinter as tk
+
+a = 0
 def click():
     global a
     a += 1
     
 
 def end():
-    txt = tk.Label(text = a)
+    txt = tk.Label(text = f"Всего кликов {a}")
     txt.pack()
     return txt
 
 
 
-import tkinter as tk
+
 
 root = tk.Tk()
 root.title("Программа для кликанья")
+root.geometry("500x500")
+
+
 
 
 btn = tk.Button(root,text = "Кликни на мне срочно", command=click)
 btn.pack()
+
 btn2 = tk.Button(root,text = "Посчитаить сколько всего кликов", command = end)
 btn2.pack()
 
 
-root.geometry("500x500")
 root.mainloop()
